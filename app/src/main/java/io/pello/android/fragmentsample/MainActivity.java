f265f396d5af4b96168787b2bea16721575fe0f2
pxai <pello_altadill@cuatrovientos.org>
2016-10-17 00:27:34 +0200

    ok added fragments

--- app/src/main/java/io/pello/android/fragmentsample/MainActivity.java
+++ app/src/main/java/io/pello/android/fragmentsample/MainActivity.java
@@ -1,13 +1,40 @@
 package io.pello.android.fragmentsample;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Si venimos de una app ya cargada, procuramos no cargar con más fragments
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        // Create an instance of ExampleFragment
+        BlankFragment blankFragment = new BlankFragment();
+
+        // En caso de haber recibido una llamada de un intent, le pasamos
+        // los parámetros extras al fragment
+        //blankFragment.setArguments(getIntent().getExtras());
+
+        // Metemos el fragment dentro del Layout
+        getSupportFragmentManager().beginTransaction().add(R.id.main_activity_framelayout, blankFragment).commit();
+
     }
+
+
+    // En caso de interacción
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.d("PELLODEBUG", "Interaction from fragment.");
+    }
+
+    
 }
